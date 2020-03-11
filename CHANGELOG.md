47bb3c26a34fa22ca7880188decdfb0e7ff31ef6
Alexandre Bulté <alexandre@bulte.net>
2020-03-11 18:06:03 +0100

    Bump version 2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Current (in progress)
+## 2.0.0 (2020-03-11)
 
 - Migrate to python3 🐍 [#61](https://github.com/etalab/udata-geoplatform/pull/61)
 
--- udata_geoplatform/__init__.py
+++ udata_geoplatform/__init__.py
@@ -5,5 +5,5 @@ Connects uData with Geoplatform (geo.data.gouv.fr)
 '''
 from __future__ import unicode_literals
 
-__version__ = '2.0.0.dev'
+__version__ = '2.0.0'
 __description__ = 'Connects uData with Geoplatform (geo.data.gouv.fr)'
